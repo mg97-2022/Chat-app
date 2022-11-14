71db0b7448cf414daf77dab0709f1882a15d95fd
mg97-2022 <mohamedgamalsaied1997@gmail.com>
2022-11-14 04:27:34 +0200

    start building signup page and signin page

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,18 @@
 import React from 'react';
+import { Route, Routes } from 'react-router-dom';
+import HomePage from './pages/HomePage/HomePage';
+import SigninPage from './pages/SigninPage/SigninPage';
+import SignupPage from './pages/SignupPage/SignupPage';
 
 function App() {
   return (
-    <div className="App">
-      home
-    </div>
+    // <SigninPage />
+    <SignupPage />
+    // <Routes>
+    //   <Route path='/' element={<HomePage />} />
+    //   <Route path='/signin' element={<SigninPage />} />
+    //   <Route path='/signup' element={<SignupPage />} />
+    // </Routes>
   );
 }
 
