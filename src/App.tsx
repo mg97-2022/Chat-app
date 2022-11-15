f0c33d006c913d7a2ca540e6b1ee6c3bb69175ee
mg97-2022 <mohamedgamalsaied1997@gmail.com>
2022-11-15 16:42:33 +0200

    building UI

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,18 @@
 import React from 'react';
 import { Route, Routes } from 'react-router-dom';
 import HomePage from './pages/HomePage/HomePage';
+import Messages from './pages/HomePage/Messages/Messages';
 import SigninPage from './pages/SigninPage/SigninPage';
 import SignupPage from './pages/SignupPage/SignupPage';
 
 function App() {
   return (
-    // <SigninPage />
-    <SignupPage />
-    // <Routes>
-    //   <Route path='/' element={<HomePage />} />
-    //   <Route path='/signin' element={<SigninPage />} />
-    //   <Route path='/signup' element={<SignupPage />} />
-    // </Routes>
+    <Routes>
+      <Route path='/' element={<HomePage />} />
+      {/* <Route path='/messages' element={<Messages />} /> */}
+      <Route path='/signin' element={<SigninPage />} />
+      <Route path='/signup' element={<SignupPage />} />
+    </Routes>
   );
 }
 
