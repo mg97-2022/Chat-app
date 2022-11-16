698744a4bcd529f48fd4d3d1537a461149c570b9
mg97-2022 <mohamedgamalsaied1997@gmail.com>
2022-11-17 00:16:06 +0200

    user signup using firebase

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,35 @@
-import React from 'react';
-import { Route, Routes } from 'react-router-dom';
-import HomePage from './pages/HomePage/HomePage';
-import Messages from './pages/HomePage/Messages/Messages';
-import SigninPage from './pages/SigninPage/SigninPage';
-import SignupPage from './pages/SignupPage/SignupPage';
+import React, { useEffect } from "react";
+import { Route, Routes } from "react-router-dom";
+import HomePage from "./pages/HomePage/HomePage";
+import Messages from "./pages/HomePage/Messages/Messages";
+import SigninPage from "./pages/SigninPage/SigninPage";
+import SignupPage from "./pages/SignupPage/SignupPage";
+
+// firebase
+import { auth } from "./firebase";
+import { onAuthStateChanged } from "firebase/auth";
+
+//
+import { useAppDispatch } from "./hooks/hooks";
+import { userSliceActions } from "./store/user";
 
 function App() {
+  const dispatch = useAppDispatch();
+
+  useEffect(() => {
+    onAuthStateChanged(auth, (user) => {
+      console.log(user);
+
+      dispatch(userSliceActions.userLogged(user));
+    });
+  }, []);
+
   return (
     <Routes>
-      <Route path='/' element={<HomePage />} />
+      <Route path="/" element={<HomePage />} />
       {/* <Route path='/messages' element={<Messages />} /> */}
-      <Route path='/signin' element={<SigninPage />} />
-      <Route path='/signup' element={<SignupPage />} />
+      <Route path="/signin" element={<SigninPage />} />
+      <Route path="/signup" element={<SignupPage />} />
     </Routes>
   );
 }
--- src/index.tsx
+++ src/index.tsx
@@ -3,14 +3,18 @@ import ReactDOM from "react-dom/client";
 import "./index.scss";
 import App from "./App";
 import { BrowserRouter } from "react-router-dom";
+import store from "./store/store";
+import { Provider } from "react-redux";
 
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
 );
 root.render(
-  <BrowserRouter>
-    <React.StrictMode>
-      <App />
-    </React.StrictMode>
-  </BrowserRouter>
+  <React.StrictMode>
+    <Provider store={store}>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </Provider>
+  </React.StrictMode>
 );
